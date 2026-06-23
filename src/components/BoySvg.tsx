212fe31c525923172d1f0ec9cb1452bfa53f6e1a
Ali Reja <al2reja@gmail.com>
2026-06-23 22:15:57 +0600

    style: enhance vector animation magnitude add padding and fix facing direction

--- src/components/BoySvg.tsx
+++ src/components/BoySvg.tsx
@@ -8,9 +8,9 @@ export default function BoySvg(props: any) {
       <svg width="0" height="0" className="absolute">
         <filter id="wavy-boy">
           <feTurbulence type="fractalNoise" baseFrequency="0.01 0.02" numOctaves="1" result="noise">
-            <animate attributeName="baseFrequency" values="0.01 0.02; 0.015 0.03; 0.01 0.02" dur="4s" repeatCount="indefinite" />
+            <animate attributeName="baseFrequency" values="0.01 0.02; 0.02 0.04; 0.01 0.02" dur="3s" repeatCount="indefinite" />
           </feTurbulence>
-          <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" xChannelSelector="R" yChannelSelector="G" />
+          <feDisplacementMap in="SourceGraphic" in2="noise" scale="8" xChannelSelector="R" yChannelSelector="G" />
         </filter>
       </svg>
 
@@ -18,7 +18,7 @@ export default function BoySvg(props: any) {
       <motion.img 
         src="/boy_transparent.png" 
         alt="Boy"
-        className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
+        className="absolute w-[90%] h-[90%] top-[10%] left-[5%] object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
         style={{ 
           clipPath: 'polygon(0 30%, 100% 30%, 100% 100%, 0 100%)',
           filter: 'url(#wavy-boy)'
@@ -29,13 +29,13 @@ export default function BoySvg(props: any) {
       <motion.img 
         src="/boy_transparent.png" 
         alt="Boy Head"
-        className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
+        className="absolute w-[90%] h-[90%] top-[10%] left-[5%] object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
         style={{ 
           clipPath: 'polygon(0 0, 100% 0, 100% 30%, 0 30%)',
           transformOrigin: '50% 30%'
         }}
-        animate={{ rotate: [0, -3, 0, 2, 0] }}
-        transition={{ repeat: Infinity, duration: 7, ease: "easeInOut" }}
+        animate={{ rotate: [0, -12, 0, 8, 0] }}
+        transition={{ repeat: Infinity, duration: 6, ease: "easeInOut" }}
       />
     </div>
   );
--- src/components/GirlSvg.tsx
+++ src/components/GirlSvg.tsx
@@ -3,14 +3,14 @@ import { motion } from 'framer-motion';
 
 export default function GirlSvg(props: any) {
   return (
-    <div className={`relative w-full h-full ${props.className || ''}`}>
+    <div className={`relative w-full h-full ${props.className || ''}`} style={{ transform: 'scaleX(-1)' }}>
       {/* Live2D Waving Filter */}
       <svg width="0" height="0" className="absolute">
         <filter id="wavy-girl">
           <feTurbulence type="fractalNoise" baseFrequency="0.01 0.02" numOctaves="1" result="noise">
-            <animate attributeName="baseFrequency" values="0.01 0.02; 0.015 0.03; 0.01 0.02" dur="5s" repeatCount="indefinite" />
+            <animate attributeName="baseFrequency" values="0.01 0.02; 0.02 0.04; 0.01 0.02" dur="3s" repeatCount="indefinite" />
           </feTurbulence>
-          <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" xChannelSelector="R" yChannelSelector="G" />
+          <feDisplacementMap in="SourceGraphic" in2="noise" scale="8" xChannelSelector="R" yChannelSelector="G" />
         </filter>
       </svg>
 
@@ -18,7 +18,7 @@ export default function GirlSvg(props: any) {
       <motion.img 
         src="/girl_transparent.png" 
         alt="Girl Torso"
-        className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
+        className="absolute w-[90%] h-[90%] top-[10%] left-[5%] object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
         style={{ 
           clipPath: 'polygon(0 32%, 100% 32%, 100% 65%, 0 65%)',
           filter: 'url(#wavy-girl)'
@@ -29,12 +29,12 @@ export default function GirlSvg(props: any) {
       <motion.img 
         src="/girl_transparent.png" 
         alt="Girl Head"
-        className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
+        className="absolute w-[90%] h-[90%] top-[10%] left-[5%] object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
         style={{ 
           clipPath: 'polygon(0 0, 100% 0, 100% 32%, 0 32%)',
           transformOrigin: '50% 32%'
         }}
-        animate={{ rotate: [0, 4, 0, -2, 0] }}
+        animate={{ rotate: [0, 10, 0, -6, 0] }}
         transition={{ repeat: Infinity, duration: 6, ease: "easeInOut" }}
       />
 
@@ -42,12 +42,12 @@ export default function GirlSvg(props: any) {
       <motion.img 
         src="/girl_transparent.png" 
         alt="Girl Legs"
-        className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
+        className="absolute w-[90%] h-[90%] top-[10%] left-[5%] object-contain pointer-events-none drop-shadow-[0_0_2px_rgba(255,255,255,0.4)]"
         style={{ 
           clipPath: 'polygon(0 65%, 100% 65%, 100% 100%, 0 100%)',
           transformOrigin: '50% 65%'
         }}
-        animate={{ rotate: [0, -5, 0, 3, 0] }}
+        animate={{ rotate: [0, -25, 0, 15, 0] }}
         transition={{ repeat: Infinity, duration: 4, ease: "easeInOut" }}
       />
     </div>
